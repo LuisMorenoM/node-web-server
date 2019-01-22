eda11af912583283158afee039d354743a5a1ad7
Luis Moreno <luismorenom95@gmail.com>
2019-01-22 17:46:03 +0100

    new gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,20 @@
 //All dependences
-var gulp 		  	= require('gulp'), //gulp
+var gulp 			= require('gulp'), //gulp
 	pump 			= require('pump'), //show js errors
 	babel			= require('gulp-babel'), //compile es6 js
 	header 			= require('gulp-header'), //add header comment
 
 	sass 			= require('gulp-sass'), //to compile sass
-    concat 			= require('gulp-concat'), //to concat
-    rename 			= require('gulp-rename'), //to rename
-    uglify 			= require('gulp-uglify'), //to uglify js
-    minify			= require('gulp-minify-css'), //to minify css
-    autoprefixer 	= require('gulp-autoprefixer'); //auto prefix(no use)
+	concat 			= require('gulp-concat'), //to concat multiple files
+	rename 			= require('gulp-rename'), //to rename
+	uglify 			= require('gulp-uglify'), //to uglify js
+	minify			= require('gulp-minify-css'), //to minify css
+	autoprefixer 	= require('gulp-autoprefixer'); //auto prefix
+	notify 			= require('gulp-notify'); //notify
 
 //paths
 var path = {
-	js 		: './public/js/base.js',
+	js 		: './public/js/**/*.js',
 	sass 	: './public/sass/base.scss',
 	build 	: './public/build'
 }
@@ -21,50 +22,53 @@ var path = {
 var pkg = require('./package.json');
 var banner = 
 	['/**',
-	  ' * <%= pkg.name %>',
-	  ' * @version v<%= pkg.version %>',
-	  ' */',
-  	''].join('\n');
+		' * <%= pkg.name %>',
+		' * @version v<%= pkg.version %>',
+		' */',
+	''].join('\n');
 
 //SASS task. Compile css, concat all in 1 and minify
 gulp.task('sass', function(){
 	gulp.src(path.sass)
-		.pipe(sass())
-		.pipe(concat('base.css'))
+		.pipe(sass({ style: 'expanded' }))
+		.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
 		.pipe(header(banner, { pkg : pkg } ))
 		.pipe(gulp.dest(path.build))
-})
-
-//minifyCSS
-gulp.task('minify-css', function(){
-	gulp.src(path.build + '/base.css')
-		.pipe(concat('base.min.css'))
+		.pipe(rename({ suffix: '.min' }))
 		.pipe(minify())
-		.pipe(header(banner, { pkg : pkg } ))
+		// .pipe(livereload(server))
 		.pipe(gulp.dest(path.build))
+		.pipe(notify({ message: 'Styles task complete' }));
 })
 
 //JS task. Compile es6 JS, concat all files in 1, uglify and rename to .min
 gulp.task('js', function(cb) {
 	pump([
-	    gulp.src(path.js)
-	    .pipe(babel({
-	        presets: ['env']
-	    }))
+		gulp.src(path.js)
+		.pipe(babel({
+			presets: ['env']
+		}))
 		.pipe(concat('base.js'))
+		.pipe(gulp.dest(path.build))
 		.pipe(uglify())
 		.pipe(rename('base.min.js'))
-		.pipe(header(banner, { pkg : pkg } )),
+		.pipe(header(banner, { pkg : pkg } ))
+		.pipe(notify({ message: 'Scripts task complete' })),
 		gulp.dest(path.build)
 	], cb );
 })
 
 //execute sass, js and minify-css
-gulp.task('all', ['sass', 'minify-css', 'js'])
+gulp.task('all', ['sass', 'js'])
 
 //watch to save
 gulp.task('watch', function() {
-	gulp.watch(path.sass, ['sass'])
-	gulp.watch(path.build + '/base.css', ['minify-css'])
-	gulp.watch(path.js, ['js'])
+	gulp.watch('./public/sass/**/*.scss', function(event) {
+		console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+		gulp.run('sass');
+	});
+	gulp.watch('./public/js/**/*.js', function(event) {
+		console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+		gulp.run('js');
+	});
 })
